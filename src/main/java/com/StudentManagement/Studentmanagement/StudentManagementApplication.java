c4e22d2fb49637a966231b5e1c24e540fc63e006
Janardhanan <2194206@cognizant.com>
2023-01-19 13:30:21 -0500

    Student management project

---
+++ src/main/java/com/StudentManagement/Studentmanagement/StudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.StudentManagement.Studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+}
